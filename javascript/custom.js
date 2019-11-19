cd412b4d9d7c3375a41ff15c16a85fb92d3c00f1
Alfredo Lopez <alfredlopezf@hotmail.com>
2019-11-19 16:08:42 +0100

    Removed lazy loaders

--- javascript/custom.js
+++ javascript/custom.js
@@ -108,26 +108,18 @@
     });
 
     jQuery(window).on('load', function () {
-        $('div#loading').fadeOut(50);
+        $('div#loading').fadeOut(0);
         window.sr = ScrollReveal({
             reset: false
         });
-        var commonCards = '#port-add-icon,.interest-icon-even,.interest-icon,' + '.timeline-dot,.timeline-content,#add-more,#skills-card,#testimonials-card,' + '#portfolios-card,#interest-card,#p-one,#p-two,#p-three,#blog-card,#contact-card,#clients,.section-title img';
-        sr.reveal(commonCards, {
-            duration: 1100
-        });
-        sr.reveal('#about-card,.map-label', {
-            duration: 1400,
-            delay: 500
-        });
-        sr.reveal('#v-card-holder', {
-            duration: 1400,
-            distance: '150px'
-        });
-        sr.reveal('.skillbar-bar', {
-            duration: 1800,
-            delay: 300,
-            distance: '0'
-        });
+        var commonCards = 
+        '#port-add-icon,.interest-icon-even,.interest-icon,' + 
+        '.timeline-dot,.timeline-content,#add-more,#skills-card,#testimonials-card,' + 
+        '#portfolios-card,#interest-card,#p-one,#p-two,#p-three,#blog-card,#contact-card,#clients,.section-title img';
+        sr.reveal(commonCards);
+        sr.reveal('#about-card,.map-label');
+
+        sr.reveal('#v-card-holder');
+        sr.reveal('.skillbar-bar');
     });
 })(jQuery);
